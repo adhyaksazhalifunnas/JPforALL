ac666496c5fba4d9ba2e970d09cfee178deb1279
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2022-10-26 11:03:20 +0700

    Add project files.

---
+++ JP_Coba1/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JP_Coba1
+{
+    class Employee
+    {
+        public int EmployeeID = 1;
+        public string LoginName;
+        public string Password;
+
+        public Employee() { }
+        public Employee(string loginName, string password)
+        {
+            LoginName = loginName;
+            Password = password;
+        }
+        public bool Login(string LoginName_, string Password_)
+        {
+            bool login = false;
+            if(LoginName == ""/*<-- Insert Username Here*/ && Password == "" /*<-- Insert Password Here*/)
+            {
+                login = true;
+            }
+            return login;
+        }
+
+    }
+}
---
+++ JP_Coba1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JP_Coba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Employee employee = new Employee();
+            employee.LoginName = tbUsername.Text;
+            employee.Password = tbPassword.Text;
+            if (employee.Login(employee.LoginName, employee.Password))
+            {
+                MessageBox.Show("Login Berhasil, ID anda : " + employee.EmployeeID.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Login Gagal");
+            }
+        }
+    }
+}
